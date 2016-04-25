7eb299ed6397a42c33e47c51ad401223230a9f9c
Ignacio Diez <ig.diez10@gmail.com>
2016-04-25 19:22:28 -0300

    Added some API consumption logic

--- app/src/main/java/devo/honstatus/MainActivity.java
+++ app/src/main/java/devo/honstatus/MainActivity.java
@@ -1,13 +1,67 @@
 package devo.honstatus;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
 import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String HON_API = 'http://api.heroesofnewerth.com';
+
+    public String readAPI(String url) {
+      StringBuilder sb = new StringBuilder();
+      HttpClient client = new DefaultHttpClient();
+      HttpGet get = new HttpGet(url);
+
+      try {
+        HttpResponse response = client.execute(get);
+        StatusLine sl = response.getStatusLine();
+        int statusCode = sl.getStatusCode();
+
+      } catch (Exception e) {
+        // Error reading data from source
+      }
+    }
+
+    private class ReadHoNFeedTask extends AsyncTask <String, Void, String>{
+
+      protected String doInBackground(String type, String player_name) {
+        // Hero Statistics
+        // Match
+        // Match History
+        // Player Statistics
+        // new HoNURL();
+        return readAPI(HON_API);
+      }
+
+      protected void onPostExecute(String result) {
+        try {
+
+        } catch (Exception e) {
+
+        }
+      }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnGetData(View view) {
+        EditText txtLat = (EditText) findViewById(R.id.txtLat);
+        EditText txtLong = (EditText) findViewById(R.id.txtLong);
+
+
+        new ReadHoNFeedTask().execute();
     }
 }
